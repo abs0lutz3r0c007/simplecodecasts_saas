e65704aedc27c3cf59525737e74afb8f768d4cc7
Newton Chan <newton.k.chan@gmail.com>
2015-05-23 02:46:59 +0000

    Added new profiles action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,3 +1,7 @@
 class PagesController < ApplicationController
-  
+  def new
+    # Form where each user can fill out their own profile
+    @user = user.find( params[:user_id] )
+    @profile = user.build_profile
+  end
 end
